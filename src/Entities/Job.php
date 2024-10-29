58b8e6f4e11420a5a74f044d0711a15b91d1527c
Link1515 <h26281660@gmail.com>
2024-10-29 14:51:32 +0800

    create Job entity

---
+++ src/Entities/Job.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Link1515\JobNotification\entities;
+
+class Job
+{
+    public string $id;
+    public string $name;
+    public string $industry;
+    public string $company;
+    public string $companyLink;
+    public string $address;
+    public ?string $landmark;
+    public float $latitude;
+    public float $longitude;
+    public string $link;
+    public string $postDate;
+    public string $description;
+    public string $salary;
+}
