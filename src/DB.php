324705b05ab6c9a303a03e3fa8356ced223d32bd
Link1515 <h26281660@gmail.com>
2024-10-30 13:36:54 +0800

    add DB class

---
+++ src/DB.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Link1515\JobNotification;
+
+use PDO;
+
+class DB
+{
+    private static ?PDO $pdo = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function createPDO(): void
+    {
+        self::$pdo = new PDO(
+            "{$_ENV['DB_DRIVER']}:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']}",
+            $_ENV['DB_USERNAME'],
+            $_ENV['DB_PASSWORD'],
+            [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false
+            ]
+        );
+    }
+
+    public static function getPDO(): PDO
+    {
+        if (is_null(self::$pdo)) {
+            self::createPDO();
+        }
+        return self::$pdo;
+    }
+}
