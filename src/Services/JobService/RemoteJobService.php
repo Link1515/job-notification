c5a1b031e08b756d574b2f3e3cbe3ad185144495
Link1515 <h26281660@gmail.com>
2024-11-01 14:32:49 +0800

    create RemoteJobService

---
+++ src/Services/JobService/RemoteJobService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Link1515\JobNotification\Services\JobService;
+
+use Link1515\JobNotification\Utils\StringUtils;
+
+class RemoteJobService extends JobService implements FetchJobByKeywordInterface
+{
+    public function fetchJobIdsByKeyword(string $keyword): array
+    {
+        $queryParams = [
+            'jobsource'  => 'joblist_search',
+            'keyword'    => $keyword,
+            'mode'       => 's',
+            'order'      => 16,
+            'page'       => 1,
+            'pagesize'   => 20,
+            'remoteWork' => 1, // 完全遠端
+            'searchJobs' => 1
+        ];
+        $url = self::LIST_URL . '?' . http_build_query($queryParams) ;
+
+        $jobs = $this->fetchJobsByUrl($url);
+        $this->filterRemoteJobs($jobs);
+        $jobIds = $this->getJobIds($jobs);
+
+        return $jobIds;
+    }
+
+    private function filterRemoteJobs(array &$jobs): void
+    {
+        $needles = ['遠端', '遠距', '居家', 'remote', 'wfh'];
+        $jobs    = array_filter($jobs, function ($job) use ($needles) {
+            $jobName = strtolower($job['jobName']);
+            return StringUtils::stringContainAny($jobName, $needles);
+        });
+    }
+}
