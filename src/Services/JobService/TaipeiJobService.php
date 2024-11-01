91849ddc45ebfc8c59bba8693cc76b9184df3251
Link1515 <h26281660@gmail.com>
2024-11-01 14:32:38 +0800

    create TaipeiJobService

---
+++ src/Services/JobService/TaipeiJobService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Link1515\JobNotification\Services\JobService;
+
+class TaipeiJobService extends JobService implements FetchJobByKeywordInterface
+{
+    public function fetchJobIdsByKeyword(string $keyword): array
+    {
+        $queryParams = [
+            'area'      => '6001001000,6001002000',
+            'jobsource' => 'joblist_search',
+            'keyword'   => $keyword,
+            'mode'      => 's',
+            'ro'        => 1, // 全職
+            'order'     => 16, // 最新更新
+            'page'      => 1,
+            'pagesize'  => 20
+        ];
+        $url = self::LIST_URL . '?' . http_build_query($queryParams) ;
+
+        $jobIds = $this->fetchJobIdsByUrl($url);
+
+        return $jobIds;
+    }
+}
