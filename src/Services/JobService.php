56a4fb63cc2298c9e7a4c92460502e4c0c70497e
Link1515 <h26281660@gmail.com>
2024-11-01 14:12:23 +0800

    change the JobService to the base class

--- src/Services/JobService.php
+++ src/Services/JobService.php
@@ -11,79 +11,24 @@ class JobService
     public const DETAILS_URL = 'https://www.104.com.tw/job/ajax/content';
     public const HEADERS     = ['Referer: https://www.104.com.tw'];
 
-    public function fetchTaipeiJobsByKeyword(string $keyword): array
+    public function fetchJobsByUrl(string $url): array
     {
-        $queryParams = [
-            'area'      => '6001001000,6001002000',
-            'jobsource' => 'joblist_search',
-            'keyword'   => $keyword,
-            'mode'      => 's',
-            'ro'        => 1, // 全職
-            'order'     => 16, // 最新更新
-            'page'      => 1,
-            'pagesize'  => 20
-        ];
-
-        $response = HttpUtils::getJson(self::LIST_URL, $queryParams, self::HEADERS);
+        $response = HttpUtils::getJson($url, [], self::HEADERS);
         $jobs     = $response['data'];
-        $jobIds   = $this->getJobIds($jobs);
+        $this->filterEngineerJobs($jobs);
 
-        return $jobIds;
+        return $jobs;
     }
 
-    public function fetchRemoteJobsByKeyword(string $keyword): array
+    public function fetchJobIdsByUrl(string $url): array
     {
-        $queryParams = [
-            'jobsource'  => 'joblist_search',
-            'keyword'    => $keyword,
-            'mode'       => 's',
-            'order'      => 16,
-            'page'       => 1,
-            'pagesize'   => 20,
-            'remoteWork' => 1, // 完全遠端
-            'searchJobs' => 1
-        ];
-
-        $response = HttpUtils::getJson(self::LIST_URL, $queryParams, self::HEADERS);
-        $jobs     = $response['data'];
-
-        $this->filterEngineerJobs($jobs);
-        $this->filterRemoteJobs($jobs);
-
+        $jobs   = $this->fetchJobsByUrl($url);
         $jobIds = $this->getJobIds($jobs);
 
         return $jobIds;
     }
 
-    public function fetchJobsByUrl(string $url): array
-    {
-        $response = HttpUtils::getJson($url, [], self::HEADERS);
-        $jobs     = $response['data'];
-        $jobIds   = $this->getJobIds($jobs);
-
-        return $jobIds;
-    }
-
-    private function filterEngineerJobs(array &$jobs): void
-    {
-        $needles = ['前端', '後端', '軟體', '工程師', 'frontend', 'backend', 'software', 'engineer', 'developer'];
-
-        $jobs = array_filter($jobs, function ($job) use ($needles) {
-            $jobName = strtolower($job['jobName']);
-            return StringUtils::stringContainAny($jobName, $needles);
-        });
-    }
-
-    private function filterRemoteJobs(array &$jobs): void
-    {
-        $needles = ['遠端', '遠距', '居家', 'remote', 'wfh'];
-        $jobs    = array_filter($jobs, function ($job) use ($needles) {
-            $jobName = strtolower($job['jobName']);
-            return StringUtils::stringContainAny($jobName, $needles);
-        });
-    }
-
-    private function getJobIds(array $jobs): array
+    protected function getJobIds(array $jobs): array
     {
         $jobIds = [];
         foreach ($jobs as $job) {
@@ -95,11 +40,21 @@ class JobService
         return $jobIds;
     }
 
-    public function fetchJobDetails(string $jobId)
+    protected function fetchJobDetails(string $jobId)
     {
         $response = HttpUtils::getJson(self::DETAILS_URL . "/{$jobId}", [], self::HEADERS);
         $details  = $response['data'];
 
         return $details;
+    }
+
+    protected function filterEngineerJobs(array &$jobs): void
+    {
+        $needles = ['前端', '後端', '軟體', '工程師', 'frontend', 'backend', 'software', 'engineer', 'developer'];
+
+        $jobs = array_filter($jobs, function ($job) use ($needles) {
+            $jobName = strtolower($job['jobName']);
+            return StringUtils::stringContainAny($jobName, $needles);
+        });
     }
 }
